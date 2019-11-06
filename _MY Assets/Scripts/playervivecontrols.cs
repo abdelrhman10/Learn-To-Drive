a030e98909f36712393e8f2323769bf5e2c431cf
abdelrhman10 <abdelrhman.magdey.10@gmail.com>
2019-11-06 17:49:50 +0200

    Removing none used code

--- _MY Assets/Scripts/playervivecontrols.cs
+++ _MY Assets/Scripts/playervivecontrols.cs
@@ -7,7 +7,7 @@ using Valve.VR.InteractionSystem;
 
 public class playervivecontrols : MonoBehaviour {
 
-    float lastWheelX, lastHandX;
+    
 
     public CircularDrive OutputDataFromSteeringWHeel;
 
@@ -15,15 +15,17 @@ public class playervivecontrols : MonoBehaviour {
 
     public float speed = 1f;
 
-    float acc = 0;
+    //float acc = 0;
     float Break = 0;
-    public bool enableforward;
-    public bool enablebackward;
+    //public bool enableforward;
+    //public bool enablebackward;
     public bool moveback = false;
 
-    public SteamVR_Action_Boolean moveForward = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "GrabPinch");
+    //public SteamVR_Action_Boolean moveForward = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "GrabPinch");
     public SteamVR_Action_Boolean movebackward = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "GrabPinch");
-    public SteamVR_Action_Boolean direction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "Teleport");
+    //public SteamVR_Action_Boolean direction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "Teleport");
+    
+    //access the triger victor 1 value
     public SteamVR_Action_Single single;
     public SteamVR_Input_Sources inputSrcs;
     // Use this for initialization
@@ -41,21 +43,13 @@ public class playervivecontrols : MonoBehaviour {
 
         float steeringwheel = OutputDataFromSteeringWHeel.outAngle * Time.deltaTime * 0.1f;
 
-
-        //if (direction[SteamVR_Input_Sources.LeftHand].stateDown)
-        //{
-        //    moveback = !moveback;
-        //}
-
-
-
         
 
-        if (moveForward[SteamVR_Input_Sources.LeftHand].state)
+        /*if (moveForward[SteamVR_Input_Sources.LeftHand].state)
         {
             acc = 1 * speed;
             Break = 0;
-        }
+       */ }
         if (direction[SteamVR_Input_Sources.LeftHand].state)
         {
             Break = -1f * speed;
